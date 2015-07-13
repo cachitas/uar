caf34e619e863f097f3d395cc3689295a7520416
Hugo Cachitas <hcachitas@gmail.com>
2015-07-13 07:01:09 +0100

    Redesigned GUI: work in progress

--- app.py
+++ app.py
@@ -1,172 +1,230 @@
-import gzip
-import io
 import os
-import re
-import shutil
-import zipfile
 import logging
+import threading
+import zipfile
+import io
 
 import tkinter as tk
-from tkinter import filedialog
 from tkinter import ttk
+from tkinter import filedialog
+from tkinter.scrolledtext import ScrolledText
+
+import uar
+from texthandler import TextHandler
 
 
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
 
-class UAR(tk.Tk):
+class ZipFileFrame(ttk.LabelFrame):
 
-    def __init__(self, parent):
+    def __init__(self, master):
+        super().__init__(master, text=' ZIP File: ')
+        self.logger = logging.getLogger(__name__)
+        self._init_ui()
 
-        tk.Tk.__init__(self, parent)
-        self.parent = parent
+        self.reset_state()
 
-        self.zip_filename = tk.StringVar(value='')
-        self.output_dir = tk.StringVar(value='')
-        self.gzip_var = tk.IntVar(value=1)
-        self.status_msg = tk.StringVar(value='')
+    def _init_ui(self):
+        self.browse_btn = ttk.Button(self,
+                                     text='Browse',
+                                     command=self._on_browse)
+        self.browse_btn.pack(side='top',
+                             padx=5, pady=5,
+                             ipadx=5, ipady=5,
+                             expand=True)
 
-        self.initialize()
+        self.extract_btn = ttk.Button(self,
+                                      text='Extract',
+                                      command=self._on_extract)
+        self.extract_btn.pack(side='top',
+                              padx=5, pady=5,
+                              ipadx=5, ipady=5,
+                              expand=True)
 
-    def initialize(self):
-        """Initialize the GUI.
-        """
-        logger.debug('Initializing the GUI')
-        self.title('Unzip Alignment Results')
-        self.minsize(width=600, height=50)
-
-        self.grid()
-
-        self.open_btn = tk.Button(self, text='Select ZIP',
-                                  command=self.askopenfilename)
-        self.open_btn.grid(column=0, row=0, sticky='WE', padx=5, pady=5)
-
-        self.open_ent = tk.Entry(self, textvariable=self.zip_filename,
-                                 state='readonly', relief='flat')
-        self.open_ent.grid(column=1, row=0, sticky='WE', padx=5, pady=5)
-
-        self.output_lbl = tk.Label(self, text='Output Location:')
-        self.output_lbl.grid(column=0, row=1, sticky='E', padx=5, pady=5)
-
-        self.output_ent = tk.Entry(self, textvariable=self.output_dir,
-                                   state='readonly', relief='flat')
-        self.output_ent.grid(column=1, row=1, sticky='WE', padx=5, pady=5)
-
-        self.options_lbl = tk.Label(self, text='Options:')
-        self.options_lbl.grid(column=0, row=2, sticky='E', padx=5, pady=5)
-
-        self.opt_gzip_chb = tk.Checkbutton(self,
-                                           text='Uncompress .gz images',
-                                           variable=self.gzip_var)
-        self.opt_gzip_chb.grid(column=1, row=2, sticky='W')
-
-        self.extract_btn = tk.Button(self, text='Extract images',
-                                     state='disabled',
-                                     command=self.extract_images)
-        self.extract_btn.grid(column=0, row=3, sticky='WE', padx=5, pady=5)
-
-        self.extract_progbar = ttk.Progressbar(self, orient='horizontal',
-                                               length=150, mode='determinate')
-        self.extract_progbar.grid(column=1, row=3, sticky='WE', padx=5, pady=5)
-
-        self.statusbar = tk.Entry(self, textvariable=self.status_msg,
-                                  state='readonly', relief='flat')
-        self.statusbar.grid(column=0, row=4, columnspan=2, sticky='WE',
-                            padx=5, pady=5)
-
-        self.grid_columnconfigure(1, weight=1)
-        self.resizable(True, False)
-
-    def askopenfilename(self):
-        """Returns the name of the file chosen.
-        """
-        file_opt = {}
-        file_opt['defaultextension'] = '.zip'
-        file_opt['filetypes'] = [('zip files', '.zip'), ('all files', '.*')]
-        file_opt['initialdir'] = '~/Downloads'
-        file_opt['initialfile'] = ''
-        file_opt['parent'] = self.parent
-        file_opt['title'] = 'Choose the zipped results'
-
-        logger.debug('Opening file dialog')
+    def _on_browse(self):
+        self.logger.debug('Opening a file dialog')
+        file_opt = dict(
+            defaultextension='.zip',
+            filetypes=[('zip files', '.zip')],
+            # initialdir='~/Downloads',
+            initialdir='.',  # for testing
+            initialfile='',
+            parent=self.master,
+            title='Choose the zipped results',
+        )
         filename = filedialog.askopenfilename(**file_opt)
 
         if filename != '':
-            self.zip_filename.set(filename)
+            self.logger.info("File '%s' selected", filename)
+            self.zipfilename = filename
             self.extract_btn.config(state='normal')
-
-            # create a folder named as the zip file to extract into
-            self.output_dir.set(os.path.splitext(self.zip_filename.get())[0])
-            logger.debug('Checking if output folder already exists')
-            if not os.path.exists(self.output_dir.get()):
-                logger.debug('Creating output folder')
-                os.mkdir(self.output_dir.get())
         else:
-            self.extract_btn.config(state='disabled')
+            self.logger.debug("Dialog canceled")
+            self.reset_state()
 
-    def extract_images(self):
-        """Extract images.
+    def _on_extract(self):
+        self.logger.debug('Starting the extraction')
+
+        # Get options values
+        gzip_var = self.master.options_frame.gzip_var
+        tofolder_var = self.master.options_frame.tofolder_var
+
+        # Inspect zip file and set progressbar maximum value
+        self.master.inspect_zipfile()
+
+        # Extract the files
+        self.master.extract_nested_zipfile()
+
+    def reset_state(self):
+        self.logger.debug("Reset state to initial values")
+        self.zipfilename = None
+        self.output_dir = None
+        self.extract_btn.config(state='disabled')
+
+
+class OptionsFrame(ttk.LabelFrame):
+
+    def __init__(self, master):
+        super().__init__(master, text=' Options: ')
+        self.logger = logging.getLogger(__name__)
+        self.gzip_var = tk.IntVar(value=1)
+        self.tofolder_var = tk.IntVar(value=1)
+        self._init_ui()
+
+    def _init_ui(self):
+        self.gzip_chb = tk.Checkbutton(self,
+                                       text='Decompress .gz images',
+                                       variable=self.gzip_var)
+        self.gzip_chb.pack(anchor='w', expand=True)
+
+        self.tofolder_chb = tk.Checkbutton(self,
+                                           text='Extract item into a folder',
+                                           variable=self.tofolder_var)
+        self.tofolder_chb.pack(anchor='w', expand=True)
+
+
+class LoggerFrame(ttk.LabelFrame):
+
+    def __init__(self, master):
+        super().__init__(master, text=' Progress: ')
+        self.logger = logging.getLogger(__name__)
+        self._init_ui()
+
+        text_handler = TextHandler(self.st)
+        self.logger.addHandler(text_handler)
+
+    def _init_ui(self):
+        self.pb = ttk.Progressbar(self)
+        self.pb.pack(padx=5, pady=5,
+                     ipadx=5, ipady=5,
+                     fill='x', expand=False)
+
+        self.st = ScrolledText(self,
+                               state='disabled',
+                               font='TkFixedFont',
+                               width=8,
+                               height=8)
+        self.st.pack(padx=5, pady=5,
+                     ipadx=5, ipady=5,
+                     fill='both', expand=True)
+
+
+class App(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.logger = logging.getLogger(__name__)
+        self._init_ui()
+
+        self.progressbar = self.logger_frame.pb
+
+    def _init_ui(self):
+        self.logger.debug('Initializing the GUI...')
+        self.title('Unzip Alignment Results')
+        self.minsize(width=500, height=200)
+
+        self.input_frame = ZipFileFrame(self)
+        self.input_frame.grid(column=0, row=0,
+                              padx=5, pady=5,
+                              ipadx=5, ipady=5,
+                              sticky='nsew')
+
+        self.options_frame = OptionsFrame(self)
+        self.options_frame.grid(column=1, row=0,
+                                padx=5, pady=5,
+                                ipadx=5, ipady=5,
+                                sticky='nsew')
+
+        self.logger_frame = LoggerFrame(self)
+        self.logger_frame.grid(column=0, row=1, columnspan=2,
+                               padx=5, pady=5,
+                               ipadx=5, ipady=5,
+                               sticky='nsew')
+
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.rowconfigure(0, weight=0)
+        self.rowconfigure(1, weight=1)
+
+    def _check_worker_thread(self, thread, logmsg=None,
+                             widget=None, widget_config={'state': 'normal'}):
+        """Check worker thread status.
+        Can also provide a `widget` to enable when thread is done working.
+        This is the default, but can be changed with the argument
+        `widget_config`.
         """
+        if thread.is_alive():
+            self.after(500, self._check_worker_thread,
+                       thread, logmsg, widget, widget_config)
+        else:
+            if logmsg is not None:
+                self.logger.info(logmsg)
+            if widget is not None:
+                widget.config(**widget_config)
 
-        # inspect the main zip file to account for inner ones
-        files_to_extract = []
-        self.status_msg.set('Inspecting...')
-        self.update_idletasks()
-        with zipfile.ZipFile(self.zip_filename.get(), 'r') as zfile:
-            for name in zfile.namelist():
-                if re.search(r'\.zip$', name, flags=re.IGNORECASE) != None:
-                    files_to_extract.append(name)
+    def inspect_zipfile(self):
+        """Inspect given zip file in order to set the progress bar
+        maximum value.
+        During this process the progress bar is in indeterminate state.
+        """
+        zf = self.input_frame.zipfilename
+        self.logger.info("Inspecting '%s'", zf)
 
-        self.extract_progbar['value'] = 0
-        self.extract_progbar['maximum'] = len(files_to_extract)
+        self.logger_frame.pb.config(mode='indeterminate')
+        self.logger_frame.pb.start()
+        inner_zfs = uar.retrieve_inner_zipfiles(zf)
+        self.logger_frame.pb.stop()
 
-        with zipfile.ZipFile(self.zip_filename.get(), 'r') as zfile:
-            for i, name in enumerate(files_to_extract):
-                self.status_msg.set('Unzipping {}'.format(
-                    os.path.basename(name)))
+        self.logger_frame.pb.config(mode='determinate',
+                                    value=0,
+                                    maximum=len(inner_zfs))
 
-                zfiledata = io.BytesIO(zfile.read(name))
-                with zipfile.ZipFile(zfiledata) as zfile2:
-                    for name2 in zfile2.namelist():
-                        filename = os.path.basename(name2)
+    def extract_nested_zipfile(self):
+        """Extract files using `uar` methodology.
+        """
+        zf = self.input_frame.zipfilename
+        pattern = r'_warped\.'
+        output_dir = os.path.splitext(zf)[0]
+        inner_zipfiles = uar.retrieve_inner_zipfiles(zf)
 
-                        # skip directories
-                        if not filename:
-                            continue
+        def extract_threaded(zf, pattern, inner_zipfiles, output_dir):
+            with zipfile.ZipFile(zf, 'r') as zfile:
+                for i, name in enumerate(inner_zipfiles):
+                    self.logger.info("Extracting '%s'", name)
+                    zfiledata = io.BytesIO(zfile.read(name))
+                    uar.extract_files(zfiledata, pattern, output_dir)
+                    self.logger_frame.pb['value'] = i + 1
 
-                        # filter files: only want those having 'wraped'
-                        if re.search(r'_warped\.', name2) is None:
-                            continue
-
-                        # copy file (taken from zipfile's extract)
-                        source = zfile2.open(name2)
-                        target = open(os.path.join(
-                            self.output_dir.get(), filename), "wb")
-                        with source, target:
-                            shutil.copyfileobj(source, target)
-
-                        # uncompress the image if asked to
-                        if self.gzip_var.get() == 1:
-                            self.uncompress_gzipped_file(target)
-
-                self.extract_progbar["value"] = i + 1
-                self.update_idletasks()
-            else:
-                self.status_msg.set('Done')
-
-    @staticmethod
-    def uncompress_gzipped_file(file_obj):
-        assert file_obj.name.endswith('.gz')
-        source = gzip.open(file_obj.name, 'rb')
-        target = open(source.name[:-3], 'wb')
-        with source, target:
-            target.write(source.read())
-        os.remove(source.name)
+        t = threading.Thread(target=extract_threaded,
+                             args=(zf, pattern, inner_zipfiles, output_dir))
+        t.start()
+        self._check_worker_thread(
+            t, logmsg="Done. Output is in '{}'".format(output_dir))
 
 
 def main():
-    app = UAR(None)
+    app = App()
     app.mainloop()
 
 
